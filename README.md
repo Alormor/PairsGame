4dddebcc1030af57edab1b89611cdd76db5530aa
Alormor <abralorenzo@gmail.com>
2025-11-11 20:05:15 +0100

    Readme update

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PAIRS GAME
-Simple mathcing pair game. There is a 4x3 board with 6 pairs to match.
+Coding a JavaScript memory matching game or pairs game is another 
+fun project you can build for fun (and for your portfolio!).
+
+The logic of the game is simple:
+
+There are a given number of cards on the table facing down. The player 
+needs to find all the pairs. However, you may want to add more difficulty 
+by limiting the number of guesses or by setting a time limit for the game 
+to be finished.
--- index.js
+++ index.js
@@ -1,15 +1,3 @@
-// PAIRS GAME
-/*
-    Coding a JavaScript memory matching game or pairs game is another 
-    fun project you can build for fun (and for your portfolio!).
-
-    The logic of the game is simple:
-
-    There are a given number of cards on the table facing down. The player 
-    needs to find all the pairs. However, you may want to add more difficulty 
-    by limiting the number of guesses or by setting a time limit for the game 
-    to be finished.
-*/
 window.onload = ()=>{
     beginPairsGame()
 }
